05725e3b6946709acdd39276922c521352975998
Sarmad-Khalique <66265577+Sarmad-Khalique@users.noreply.github.com>
2020-10-01 13:17:14 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,12 +2,13 @@
 import sys
 
 def DoesNotExist(name):
-	print name, "does not exist"
+	print(name, "does not exist")
 
 def Hello(name):
 	if name == "Alice" or name == "Nick":
+		# Concatenting Strings
 		name = name + "!!!!"
-		print "Hello", name
+		print("Hello", name)
 	else:
 		DoesNotExist(name)
 	
